a613fdf66c7c32860160dd0eea722630e1ce630f
arno <1017845778@qq.com>
2021-12-23 17:04:14 +0800

    first commit

---
+++ output/DefineID.h
@@ -0,0 +1,79 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+//创建画面Screen1的命令索引ID
+#define  _SCREEN_SCREEN1                                                       1
+
+#define  _RECORD_SCREEN0_RECORD1                                               1
+
+#define  _ANIMATION_SCREEN0_ICON1                                              4
+
+//画面Screen0中动画控件Icon2使用的图片
+#define  _IMG_SCREEN0_ICON2                                                    0
+
+#define  _ANIMATION_SCREEN0_ICON2                                              5
+
+#define  _BTN_SCREEN0_BUTTON1                                                  6
+
+//画面Screen1中按钮Button1按下时的图片
+#define  _IMG_SCREEN1_BUTTON1_DOWN                                             2
+
+#define  _BTN_SCREEN1_BUTTON1                                                  1
+
+//画面Screen1中按钮Button2按下时的图片
+#define  _IMG_SCREEN1_BUTTON2_DOWN                                             3
+
+#define  _BTN_SCREEN1_BUTTON2                                                  6
+
+//画面Screen1中动画控件Icon1使用的图片
+#define  _IMG_SCREEN1_ICON1                                                    0
+
+#define  _ANIMATION_SCREEN1_ICON1                                              4
+
+//画面Screen1中动画控件Icon2使用的图片
+#define  _IMG_SCREEN1_ICON2                                                    0
+
+#define  _ANIMATION_SCREEN1_ICON2                                              5
+
+#define  _RECORD_SCREEN1_RECORD1                                               7
+
+#define  _BTN_SCREEN1_BUTTON3                                                  8
+
+#define  _BTN_SCREEN1_BUTTON4                                                  9
+
+#define  _BTN_SCREEN1_BUTTON5                                                 10
+
+#define  _BTN_SCREEN1_BUTTON6                                                 11
+
+#define  _BTN_SCREEN1_BUTTON7                                                 12
+
+#define  _BTN_SCREEN1_BUTTON8                                                 13
+
+#define  _BTN_SCREEN1_BUTTON9                                                 14
+
+#define  _BTN_SCREEN1_BUTTON10                                                15
+
+#define  _BTN_SCREEN1_BUTTON11                                                16
+
+#define  _BTN_SCREEN1_BUTTON12                                                17
+
+#define  _BTN_SCREEN1_BUTTON13                                                18
+
+#define  _BTN_SCREEN1_BUTTON14                                                19
+
+#define  _BTN_SCREEN1_BUTTON15                                                20
+
+#define  _BTN_SCREEN1_BUTTON16                                                21
+
+#define  _BTN_SCREEN1_BUTTON17                                                22
+
+#define  _BTN_SCREEN1_BUTTON18                                                23
+
+#define  _BTN_SCREEN1_BUTTON19                                                24
+
+#define  _BTN_SCREEN1_BUTTON20                                                25
+
+#define  _BTN_SCREEN1_BUTTON21                                                26
+
+#define  _BTN_SCREEN1_BUTTON22                                                27
+
