9b4df69b0bd84ed6f9c975d3cb1e08f9e72342ae
arno <1017845778@qq.com>
2022-01-18 17:47:23 +0800

    增加信号大于20

--- output/DefineID.h
+++ output/DefineID.h
@@ -15,6 +15,12 @@
 
 #define  _BTN_SCREEN0_BUTTON1                                                  6
 
+#define  _BTN_SCREEN0_BUTTON2                                                  7
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY1                                       8
+
+#define  _BTN_SCREEN0_BUTTON3                                                  9
+
 #define  _BTN_SCREEN1_BUTTON1                                                  1
 
 #define  _BTN_SCREEN1_BUTTON2                                                  6
@@ -30,4 +36,20 @@
 #define  _ANIMATION_SCREEN1_ICON2                                              5
 
 #define  _RECORD_SCREEN1_RECORD1                                               7
+
+//画面Screen1中按钮Button3弹起时的图片
+#define  _IMG_SCREEN1_BUTTON3_UP                                               1
+
+//画面Screen1中按钮Button3按下时的图片
+#define  _IMG_SCREEN1_BUTTON3_DOWN                                             2
+
+#define  _BTN_SCREEN1_BUTTON3                                                  8
+
+//画面Screen1中按钮Button4弹起时的图片
+#define  _IMG_SCREEN1_BUTTON4_UP                                               3
+
+//画面Screen1中按钮Button4按下时的图片
+#define  _IMG_SCREEN1_BUTTON4_DOWN                                             4
+
+#define  _BTN_SCREEN1_BUTTON4                                                  9
 
